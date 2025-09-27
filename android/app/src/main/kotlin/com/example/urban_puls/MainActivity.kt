ec6ab829040da3ef4b4fb8002d79003c486dda64
Vishal <vishal9345566@gmail.com>
2025-09-27 19:24:47 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/urban_puls/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.urban_puls
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/Screens/welcomScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Welcomscreen extends StatefulWidget {
+  const Welcomscreen({super.key});
+
+  @override
+  State<Welcomscreen> createState() => _WelcomscreenState();
+}
+
+class _WelcomscreenState extends State<Welcomscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
